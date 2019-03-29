6915fa995b45bc8216b39195f7c35315e5c7a7cd
Escobar Silva <000404669@bga.upb>
2019-03-29 11:53:02 -0500

    Primer commit

--- Ejercicio1.py
+++
@@ -1,7 +0,0 @@
-n=1
-contador=0
-while n <=30:
-    if n%2==0:
-        contador=contador+1
-        print(n)
-    n=n+1    
--- Ejercicio3.py
+++
@@ -1,7 +0,0 @@
-mayor=int(0)
-for i in range(0, 5):
-    x=int(input("ingrese los numeros"))
-    if x >= mayor:
-        mayor = x
-    print("el mayor es: ", mayor)
-
